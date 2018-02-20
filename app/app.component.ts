82cee6a46b968a23bd0fc47932ded60bcb10acf0
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-02-20 10:59:37 -0300

    Test directives

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-	title = 'My First Angular App';
+	title = 'app';
+
+	list = [
+		{
+			name: 'Tour of Heroes',
+			href: 'https://angular.io/tutorial'
+		}, {
+			name: 'CLI Documentation',
+			href: 'https://github.com/angular/angular-cli/wiki'
+		}, {
+			name: 'Angular blog',
+			href: 'https://blog.angular.io/'
+		}
+	];
+
+	texts = [];
+
+	text = '';
+
+	add() {
+		this.texts.push(this.text);
+		this.text = '';
+	}
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   	],
   	
   	imports: [
-	    BrowserModule
+	    BrowserModule,
+        FormsModule
   	],
   	
   	providers: [],
