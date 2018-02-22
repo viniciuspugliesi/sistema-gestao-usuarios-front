f27805031169c3aee566d54779ea4cbadf1bde5d
viniciuspugliesi <vinicius_pugliesi@outlook.com>
2018-02-22 16:06:57 -0300

    Test routeLink navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 
@@ -12,6 +13,12 @@ import { ContactListComponent } from './contact-list/contact-list.component'
 import { ContactService } from './services/contact.service'
 import { AddressService } from './services/address.service'
 import { AddressComponent } from './address/address.component'
+
+const routes:Routes = [
+	{path: 'contacts', component: ContactListComponent},
+	{path: 'contacts/create', component: ContactFormComponent},
+	{path: 'search-for-area-code', component: AddressComponent},
+]
 
 @NgModule({
 	declarations: [
@@ -28,6 +35,7 @@ import { AddressComponent } from './address/address.component'
 	    BrowserModule,
 		FormsModule,
 		HttpClientModule,
+		RouterModule.forRoot(routes)
   	],
   	
   	providers: [
